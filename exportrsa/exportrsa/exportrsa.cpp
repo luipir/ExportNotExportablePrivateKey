ee3cfae504a409663fa32d14c47ce58c18ac55bf
Luigi Pirelli <luipir@gmail.com>
2016-07-27 14:23:54 +0200

    updated project and debug bild

--- exportrsa/exportrsa/exportrsa.cpp
+++ exportrsa/exportrsa/exportrsa.cpp
@@ -74,45 +74,45 @@ BOOL g_fWow64Process;
 
 wstring
 getpass(
-    const char *prompt,
-    bool show_asterisk=true)
+	const char *prompt,
+	bool show_asterisk=true)
 {
-    const char BACKSPACE=8;
-    const char RETURN=13;
+	const char BACKSPACE=8;
+	const char RETURN=13;
 
-    wstring password;
-    unsigned char ch=0;
+	wstring password;
+	unsigned char ch=0;
 
-    cout <<prompt << endl;
+	cout <<prompt << endl;
 
-    DWORD con_mode;
-    DWORD dwRead;
+	DWORD con_mode;
+	DWORD dwRead;
 
-    HANDLE hIn=GetStdHandle(STD_INPUT_HANDLE);
+	HANDLE hIn=GetStdHandle(STD_INPUT_HANDLE);
 
-    GetConsoleMode( hIn, &con_mode );
-    SetConsoleMode( hIn, con_mode & ~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT) );
+	GetConsoleMode( hIn, &con_mode );
+	SetConsoleMode( hIn, con_mode & ~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT) );
 
-    while( ReadConsoleA( hIn, &ch, 1, &dwRead, NULL) && ch !=RETURN )
-    {
-        if(ch==BACKSPACE)
-        {
-            if(password.length()!=0)
-            {
-                if(show_asterisk)
-                    cout <<"\b \b";
-                password.resize(password.length()-1);
-            }
-        }
-        else
-        {
-            password+=ch;
-            if(show_asterisk)
-                cout <<'*';
-        }
-    }
-    cout <<endl;
-    return password;
+	while( ReadConsoleA( hIn, &ch, 1, &dwRead, NULL) && ch !=RETURN )
+	{
+		if(ch==BACKSPACE)
+		{
+			if(password.length()!=0)
+			{
+				if(show_asterisk)
+					cout <<"\b \b";
+				password.resize(password.length()-1);
+			}
+		}
+		else
+		{
+			password+=ch;
+			if(show_asterisk)
+				cout <<'*';
+		}
+	}
+	cout <<endl;
+	return password;
 }
 
 BOOL WINAPI
@@ -543,7 +543,7 @@ CertEnumSystemStoreCallback(
                     NULL);
         CloseHandle(hFile);
 
-        fprintf(stdout, "SUCCESSFULLY exported cert bundle for \"%S\"in file \"%S\" \n\n", dwCertName, wszFileName );
+		fprintf(stdout, "SUCCESSFULLY exported cert bundle for \"%S\"in file \"%S\" \n\n", dwCertName, wszFileName );
     }
     return TRUE;
 }
